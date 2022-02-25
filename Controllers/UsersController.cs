5a7f80258aab80a26b2e9c830aad0f07ccd07654
YugandharaChaudhari <95269257+YugandharaChaudhari@users.noreply.github.com>
2022-02-25 13:08:18 +0530

    userContoller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyntraClone.Models;
+using MyntraDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyntraClone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        UserRepository repository = new UserRepository();
+        dev_MyntradbContext context = new dev_MyntradbContext();
+
+        [HttpGet]
+        public IEnumerable<User> get()
+        {
+            return repository.getUser().ToList();
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] User user)
+        {
+            //var id = _repository.getCount();
+            //user.Id = id + 1;
+            if (ModelState.IsValid)
+            {
+                //Guid obj = Guid.NewGuid();
+                // employee.Id = obj();
+                repository.AddUser(user);
+                return Ok(user);
+            }
+            return BadRequest();
+        }
+
+    }
+}
+
---
+++ Models/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyntraDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyntraClone.Models
+{
+    public class UserRepository
+    {
+        dev_MyntradbContext context = new dev_MyntradbContext();
+
+        public IEnumerable<User> getUser()
+        {
+            var users = context.Users.ToList();
+            return users;
+        }
+        public void AddUser(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+
+        }
+    }
+}
